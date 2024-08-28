c63ef1e486e10e8c4d14843661f1c541d6d328eb
codyhighroller <123840075+codyhighroller@users.noreply.github.com>
2024-08-28 11:06:11 +0100

    change prod environment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "https://drawn-together.netlify.app",
+		origin: "https://drawntogether-backend.onrender.com/",
 		methods: ["GET", "POST"],
 	},
 });
