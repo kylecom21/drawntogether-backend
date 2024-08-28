4020281d23f56ca798cbefae3bff75d27d637e37
codyhighroller <123840075+codyhighroller@users.noreply.github.com>
2024-08-28 11:19:10 +0100

    update server origin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "https://drawntogether-backend.onrender.com/",
+		origin: "https://drawn-together.netlify.app",
 		methods: ["GET", "POST"],
 	},
 });
